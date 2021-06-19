55335754570833472487274aa0710d1817bbb24d
Changhee Park <arrans9591@gmail.com>
2021-06-19 16:20:00 +0900

    auth.js 내용 수정 :star:

--- 06LoginRouting/auth.js
+++ 06LoginRouting/auth.js
@@ -4,10 +4,14 @@ const users = [
     {email:"park@test.com" , password : "789" , name : "Park"},
 ];
 
-export function signIn( {eamil, password} ){
+export function signIn( {email, password} ){
+    //users => DB
+    // DB에서 사용자가입력한 email과 password가 일치한 user를 찾는다.
     const user = users.find(
-        (user)=> user.email === email && user.password === password
+        (user) => user.email === email && user.password === password
     );
+    // 참일경우 해당 user retrun
+    // 거짓일 경우 ERROR
 
     if(user === undefined) throw new Error();
     return user;
